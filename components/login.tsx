4e3f440d4c6766f6ec39dba41bb78ac233089121
hro-maker <hrant.muradyan.1998@mail.ru>
2021-05-26 08:16:43 +0400

    initial

---
+++ components/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <div className="df">
+                <div className="login_left">kajsdh</div>
+                <div className="login_rigth">askjdhkajsl</div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+
+import { AppProps } from 'next/app';
+import { FC } from 'react';
+import '../styles/index.scss'
+
+const MyApp: FC<AppProps> = ({Component, pageProps}) => {
+    return <Component {...pageProps} />
+  }
+  export default MyApp
