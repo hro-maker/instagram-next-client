625e070a8bf8061bf715fa56c0fdb4e18cb08593
hro-maker <hrant.muradyan.1998@mail.ru>
2021-05-26 09:58:55 +0400

    slider

--- components/login.tsx
+++ components/login.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
 const Login = () => {
+    const [slider,setSlider]=useState<number>(0)
+    setInterval(()=>{
+            if(slider >= 3){
+                setSlider(0)
+            }else{
+                setSlider(prev=>prev+1)
+            }
+    },3000)
+    useEffect(() => {
+        console.log(slider)
+    }, [slider]);
     return (
-        <div>
-            <div className="df">
-                <div className="login_left">kajsdh</div>
+        <Container className="container" maxWidth='md' >
+            <div className="df login_wraper" >
+                <div className="login_left">
+                    <div className="phone_wraper">
+                      <div className="phone_items">
+                        <div style={slider === 0 ?{opacity:1} : {opacity:0} } className="fisrt_item"></div>
+                        <div style={slider === 1 ?{opacity:1} : {opacity:0} } className="second_item"></div>
+                        <div style={slider === 2 ?{opacity:1} : {opacity:0} } className="third_item"></div>
+                        <div style={slider === 3 ?{opacity:1} : {opacity:0} } className="fourth_item"></div>
+                        </div>
+                    </div>
+                </div>
                 <div className="login_rigth">askjdhkajsl</div>
             </div>
-        </div>
+        </Container>
     );
 }
 
