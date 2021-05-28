cda25bbfd44c0e0c23e491d8cda304a4917a8e6b
hro-maker <hrant.muradyan.1998@mail.ru>
2021-05-28 19:14:37 +0400

    images

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
+// const macbookIphone = require('./heartt.png');
+import heart from './heartt.png'
+import home from './homee.png'
+import message from './messages.png'
+import user from './user.png'
+
 const Header = () => {
     return (
         <div className="header_big_wraper">
             <div className="header_container">
-               <div className="header_small_wraper">
-               <div className="header_left"><div className="header_logo"></div></div>
-                <div className="header_center">
-                    <input className="header_search_input" placeholder="🔎︎Search" type="text" />
+                <div className="header_small_wraper">
+                    <div className="header_left"><div className="header_logo"></div></div>
+                    <div className="header_center">
+                        <input className="header_search_input" placeholder="🔎︎Search" type="text" />
+                    </div>
+                    <div className="header_rigth">
+                        
+                       <img className="header_icons" src={home} alt='Home Page' width={25} height={25} />
+                       <img className="header_icons" src={message} alt='Home Page' width={25} height={25} />
+                       <img className="header_icons" src={heart} alt='Home Page' width={25} height={25} />
+                       <img className="header_icons" src={user} alt='Home Page' width={25} height={25} />
+                          </div>
                 </div>
-                <div className="header_rigth">
-                        <div className="logo_home"></div>
-                        <div className="logo_message"></div>
-                        <div className="logo_like"></div>
-                        <img className="log_user"src='https://www.pngjoy.com/pngm/810/9598635_person-icon-png-male-placeholder-png-download.png' alt="kjasdhk" />
-                </div>
-               </div>
-                </div>
-                
+            </div>
+
         </div>
     );
 }
