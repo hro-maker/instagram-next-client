512e607252719fc7093fc7fc80ccd5ba471664a6
hro-maker <hrant.muradyan.1998@mail.ru>
2021-06-03 09:15:06 +0400

    drop doun

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,9 +6,15 @@ import message from './messages.png'
 import user from './user.png'
 import { imageUrl } from './../../helpers/urls';
 import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
+import { destroyCookie } from 'nookies';
 
 const Header = ({avatar,_id}:any) => {
     const router=useRouter()
+    const logout=()=>{
+        destroyCookie(null, 'token')
+        router.push('/login')
+    }
     return (
         <div className="header_big_wraper">
             <div className="header_container">
@@ -23,9 +29,24 @@ const Header = ({avatar,_id}:any) => {
                        <img className="header_icons" src={message} alt='Home Page' width={25} height={25} />
                        <img className="header_icons" src={heart} alt='Home Page' width={25} height={25} />
                        <img onClick={()=>router.push('/profile/'+_id)} className="header_icons" src={avatar ?imageUrl+avatar :user} alt='Home Page' width={25} height={25} />
+                       <div className="header_dropdoun">
+                               <Link href={`/profile/${_id}`}>
+                              <a  className="header_dropdaoum-item"> <div><span></span> profile</div></a>
+                               </Link>
+                               <Link href={`/`}>
+                              <a className="header_dropdaoum-item"> <div ><span></span>Edit profile</div></a>
+                               </Link><Link href={`/`}>
+                              <a className="header_dropdaoum-item"> <div ><span></span>saved</div></a>
+                               </Link>
+                            
+                              <a className="header_dropdaoum-item"> <div onClick={logout} ><span></span>Logout</div></a>
+                               
+                         </div>
                           </div>
+
                 </div>
             </div>
+            
 
         </div>
     );
