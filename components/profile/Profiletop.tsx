d06a6fce38e96c1a84c2fa46e6d6e36d99b2d02f
hro-maker <hrant.muradyan.1998@mail.ru>
2021-06-03 21:04:13 +0400

    modal

--- components/profile/Profiletop.tsx
+++ components/profile/Profiletop.tsx
@@ -38,7 +38,7 @@ const Profiletop = ({ other: userrr }: { other: userr }) => {
     // }, [])
     return (
         <div className="profile_information">
-            <Usersmodal userId={user._id} type="u"/>
+            {/* <Usersmodal userId={user._id} type="u"/> */}
             <div className="image_wraper">
                 <div className="image_item">
                     <img className="profile_image" width="180px" height="180px" src={other.avatar.length > 1 ? imageUrl + other.avatar : userimage} alt="avatar" />
--- components/profile/profilemodal.tsx
+++ components/profile/profilemodal.tsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { Api } from './../../utiles/api';
 import { parseCookies } from 'nookies';
-interface subscruser{
-    name:string
-    surename:string
-    avtar:string
-    _id:string
+import userImage from '../header/user.png'
+import { imageUrl } from './../../helpers/urls';
+import Link from 'next/link';
+import { useAppSelector } from '../../hooks/redux';
+interface subscruser {
+    name: string
+    surename: string
+    avtar: string
+    _id: string
 }
-const Usersmodal = ({userId,type="i"}:{userId:string,type:string}) => {
-    const cookies=parseCookies()
+const Usersmodal = ({ userId, type = "i" }: { userId: string, type: string }) => {
+    const cookies = parseCookies()
     const [subscripers, setsubscripers] = useState<subscruser[]>([]);
-    useEffect(()=>{
-            (async()=>{
-                let userss=[]
-                if(type==="i"){
-                    userss=await Api({},cookies.token).getIsubscrip(userId)
-                }else{
-                   userss=await Api({},cookies.token).getother(userId)
-                }
-                setsubscripers(userss)
-            })()
-    },[])
+    useEffect(() => {
+        (async () => {
+            let userss = []
+            if (type === "i") {
+                userss = await Api({}, cookies.token).getIsubscrip(userId)
+            } else {
+                userss = await Api({}, cookies.token).getother(userId)
+            }
+            console.log(userss)
+            setsubscripers(userss)
+        })()
+    }, [])
+    const user=useAppSelector(state=>state.user.user)
+    
     return (
         <div className="subscripers_modal_overlay">
-                <div className="subscripers_modal-body">hello</div>
+            <div className="subscripers_modal-body">
+                {
+                    subscripers.map(el =>
+                        <div className="subscrip_post">
+                            <img className="modal_othertop-avatar profile-userimage" src={el.avtar ? imageUrl + el.avtar : userImage} alt="sssss" />
+                            <div>
+                               <Link href={`/profile/${el._id}`}>
+                                   <a  className="profile_modal-username"> {el.name} {el.surename}</a>
+                                </Link>
+                            </div>
+                            <button>subscr</button>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 }
